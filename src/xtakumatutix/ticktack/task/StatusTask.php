9eb193d5575753221ed2b2d6b4e3df77a4678196
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2020-09-27 03:55:04 +0900

    ちょっとオサレに

--- src/xtakumatutix/ticktack/task/StatusTask.php
+++ src/xtakumatutix/ticktack/task/StatusTask.php
@@ -22,9 +22,10 @@ class StatusTask extends Task{
             $id = $item->getId();
             $damage = $item->getDamage();
             $ping = $player->getPing();
-
+            $cloor = mt_rand(0, 9);
+            
             $api = EasyScoreboardAPI::getInstance();
-            $api->sendScoreBoard($player, "sidebar", "§gTickTack!!", false);
+            $api->sendScoreBoard($player, "sidebar", "§".$cloor."TickTack!!", false);
             $api->setScore($player, "sidebar", "§b今日は、§f".$time."§bです", 0 , 0);
             $api->setScore($player, "sidebar", "§a座標 §f>> ".$x.",".$y.",".$z." ".$level, 1 , 1);
             $api->setScore($player, "sidebar", "§e所持金 §f>> ".$money."§e円", 2 , 2);
